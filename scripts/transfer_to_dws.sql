db57c2046e03c22730948d580e2f9503da9ea3bf
dadadaniil <115701903+dadadaniil@users.noreply.github.com>
2024-06-07 19:20:23 +0300

    Update transfer_to_dws.sql

--- scripts/transfer_to_dws.sql
+++ scripts/transfer_to_dws.sql
@@ -6,19 +6,21 @@ DECLARE
 BEGIN
     FOR bank_data IN SELECT name, iban, foundation_date, address, capitalization FROM postgres.public.Bank
     LOOP
-        -- Insert into public_information table and get the id
         INSERT INTO bank_dws.public.public_information (foundation_date, address, iban)
         VALUES (bank_data.foundation_date, bank_data.address, bank_data.iban)
         RETURNING id INTO public_information_id;
 
-        -- Insert into capitalization table and get the id
         INSERT INTO bank_dws.public.capitalization (capitalization)
         VALUES (bank_data.capitalization)
         RETURNING id INTO capitalization_id;
 
-        -- Insert into bank table
         INSERT INTO bank_dws.public.bank (public_information, accounting)
         VALUES (public_information_id, capitalization_id);
     END LOOP;
 END;
 $$ LANGUAGE plpgsql;
+
+CREATE TRIGGER transfer_data_to_dws_trigger
+    AFTER UPDATE ON postgres.public."Bank"
+    FOR EACH ROW
+    EXECUTE PROCEDURE transfer_data_to_dws();
